c75bcdfa1a1bf5abddaa19dafd7c6fab40917841
DoTienOanh <164726088+DoTienOanh@users.noreply.github.com>
2024-03-24 22:03:39 +0700

    irisclassifier.py

--- irisclassifier.py
+++ irisclassifier.py
@@ -4,22 +4,17 @@ from sklearn.model_selection import train_test_split
 import pickle as pickle
 
 iris = load_iris()
-X, y =iris.data, iris.target
+X, y = iris.data, iris.target
 
-print(X.shape)
-#print(y.shape)
 
-X_train, X_test, y_train, y_test =train_test_split(X,y,test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 print(X_train.shape)
 print(y_train.shape)
 
-clf=RandomForestClassifier()
+clf = RandomForestClassifier()
 clf.fit(X_train, y_train)
 
 print(clf.score(X_test, y_test))
 
-print('Saving model to pickle file')
-pickle.dump(clf, open("iris_model.pkl", 'wb'))
-
-#new_input=[5, 3.1,6,2].reshape(-1,1)
-#print(clf.predict(new_input))
+print("Scroring model to pickle file")
+pickle.dump(clf,open("iris_model.pkl", 'wb'))
