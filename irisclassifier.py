ee39b691850bf34ba96d4e98849dbd161e47ecb6
DoTienOanh <164726088+DoTienOanh@users.noreply.github.com>
2024-03-24 22:41:05 +0700

    irisclassifier.py

--- irisclassifier.py
+++ irisclassifier.py
@@ -1,21 +1,31 @@
-pip install -r requirements.txt
-from sklearn.datasets import load_iris
+#from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import pickle as pickle
+import pandas as pd
 
-iris = load_iris()
-X, y = iris.data, iris.target
+# Đường link đến dữ liệu trên GitHub
+url = "https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv"
+# Đọc dữ liệu vào DataFrame
+iris = pd.read_csv(url)
 
+#iris = load_iris()
+X, y =iris.data, iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+print(X.shape)
+#print(y.shape)
+
+X_train, X_test, y_train, y_test =train_test_split(X,y,test_size=0.2)
 print(X_train.shape)
 print(y_train.shape)
 
-clf = RandomForestClassifier()
+clf=RandomForestClassifier()
 clf.fit(X_train, y_train)
 
 print(clf.score(X_test, y_test))
 
-print("Scroring model to pickle file")
-pickle.dump(clf,open("iris_model.pkl", 'wb'))
+print('Saving model to pickle file')
+pickle.dump(clf, open("iris_model.pkl", 'wb'))
+
+#new_input=[5, 3.1,6,2].reshape(-1,1)
+#print(clf.predict(new_input))
